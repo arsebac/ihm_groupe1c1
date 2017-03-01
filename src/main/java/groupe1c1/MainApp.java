76029551ff8e16156610608442381443151d5e47
AlexisDeslandes <deslandes.alexis@live.fr>
2017-03-01 15:10:15 +0100

    Quelques modifications de position

--- src/main/java/groupe1c1/MainApp.java
+++ src/main/java/groupe1c1/MainApp.java
@@ -3,7 +3,6 @@ package groupe1c1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
@@ -38,7 +37,7 @@ public class MainApp extends Application {
 
     }
     private Node loadTop() throws IOException {
-        String fxmlFile = "/fxml/TOP.fxml";
+        String fxmlFile = "/fxml/topFinal.fxml";
         FXMLLoader loader = new FXMLLoader();
         return loader.load(getClass().getResourceAsStream(fxmlFile));
     }
