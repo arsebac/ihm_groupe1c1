3edf585da10e20c02689ebf66fd9a46fbc8f3afe
Francois Melkonian <francois.melkonian@etu.unice.fr>
2017-03-01 08:25:25 +0100

    Assemblage des différentes parties

--- src/main/java/groupe1c1/MainApp.java
+++ src/main/java/groupe1c1/MainApp.java
@@ -2,11 +2,16 @@ package groupe1c1;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class MainApp extends Application {
 
@@ -19,18 +24,32 @@ public class MainApp extends Application {
     public void start(Stage stage) throws Exception {
 
         log.info("Starting Hello JavaFX and Maven demonstration application");
-
-        String fxmlFile = "/fxml/produit.fxml";
-        log.debug("Loading FXML for main view from: {}", fxmlFile);
-        FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-
+        VBox parent = new VBox();
+        parent.getChildren().add(loadTop());
+        parent.getChildren().add(loadProduitPhares());
+        parent.getChildren().add(loadMagasins());
         log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 1280, 400);
+	    ScrollPane scrollPane = new ScrollPane(parent);
+	    Scene scene = new Scene(scrollPane, 1283, 720);
         scene.getStylesheets().add("/styles/styles.css");
         stage.setTitle("Hello JavaFX and Maven");
         stage.setScene(scene);
         stage.show();
 
+    }
+    private Node loadTop() throws IOException {
+        String fxmlFile = "/fxml/TOP.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        return loader.load(getClass().getResourceAsStream(fxmlFile));
+    }
+    private Node loadProduitPhares() throws IOException {
+        String fxmlFile = "/fxml/produit.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        return loader.load(getClass().getResourceAsStream(fxmlFile));
+    }
+    private Node loadMagasins() throws IOException {
+        String fxmlFile = "/fxml/magasin-vu-global.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        return loader.load(getClass().getResourceAsStream(fxmlFile));
     }
 }
