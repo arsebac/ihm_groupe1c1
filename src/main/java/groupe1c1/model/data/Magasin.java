0a8fce7198d6aa85e478414e4b837e87d0e3d4dc
David Lang <david.lang@etu.unice.fr>
2017-03-11 11:24:21 +0100

    Magasin temporaire qui marche

--- src/main/java/groupe1c1/model/data/Magasin.java
+++ src/main/java/groupe1c1/model/data/Magasin.java
@@ -10,14 +10,20 @@ public class Magasin implements Serializable {
 
     private String Name;
     private String pathImage;
-    private String horaires;
+    private String centreCommercial;
     private String adresse;
+    private String telephone;
+    private String mail;
+    private String siteWeb;
 
-    public Magasin(String name, String pathImage, String horaires, String adresse) {
+    public Magasin(String name, String pathImage, String centreCommercial, String adresse, String telephone, String mail, String siteWeb) {
         Name = name;
         this.pathImage = pathImage;
-        this.horaires = horaires;
+        this.centreCommercial = centreCommercial;
         this.adresse = adresse;
+        this.telephone = telephone;
+        this.mail = mail;
+        this.siteWeb = siteWeb;
     }
 
     public String getName() {
@@ -26,10 +32,6 @@ public class Magasin implements Serializable {
 
     public final String getPathImage() {
         return pathImage;
-    }
-
-    public final String getHoraires() {
-        return horaires;
     }
 
     public final String getAdresse() {
@@ -43,12 +45,40 @@ public class Magasin implements Serializable {
     public void setPathImage(String pathImage) {
         this.pathImage = pathImage;
     }
-
-    public void setHoraires(String horaires) {
-        this.horaires = horaires;
-    }
-
+    
     public void setAdresse(String adresse) {
         this.adresse = adresse;
+    }
+
+    public String getCentreCommercial() {
+        return centreCommercial;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getSiteWeb() {
+        return siteWeb;
+    }
+
+    public void setCentreCommercial(String centreCommercial) {
+        this.centreCommercial = centreCommercial;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setSiteWeb(String siteWeb) {
+        this.siteWeb = siteWeb;
     }
 }
